03c390f57d4c2350ae513e290931031658ab2c19
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-10-20 20:06:57 +0300

    fixted bag

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Skills.module.css';
+import Skill from "./skill";
 
 function Skills() {
   return (
@@ -7,27 +8,11 @@ function Skills() {
       <div className={styles.header}>
           <span>Мои скилы</span>
           <div className={styles.mainContent}>
-              <div className={styles.contentOne}>
-                  <div className={styles.photoOne}></div>
-                  <span>react</span>
-                  <div className={styles.innerOne}>
-                      <div> <span>Подробное описание навыка</span> </div>
-                  </div>
-              </div>
-              <div className={styles.contentTwo}>
-                  <div className={styles.photoTwo}></div>
-                  <span>react</span>
-                  <div className={styles.innerTwo}>
-                      <div> <span>Подробное описание навыка</span> </div>
-                  </div>
-              </div>
-              <div className={styles.contentThree}>
-                  <div className={styles.photoThree}></div>
-                  <span>react</span>
-                  <div className={styles.innerThree}>
-                      <div> <span>Подробное описание навыка</span> </div>
-                  </div>
-              </div>
+
+              <Skill/>
+              <Skill/>
+              <Skill/>
+
           </div>
       </div>
     </div>
---
+++ src/Skills/skill.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Skills.module.css';
+
+function Skill() {
+    return (
+
+
+        <div className={styles.mainContent}>
+            <div className={styles.skill}>
+                <div className={styles.photo}></div>
+                <span>react</span>
+                <div className={styles.inner}>
+                    <div><span>Подробное описание навыка</span></div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+export default Skill;
