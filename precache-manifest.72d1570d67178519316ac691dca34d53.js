ac3878c6044929baec93a0d0f85940f40f15b9c7
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-11-03 02:18:42 +0300

    Updates

---
+++ precache-manifest.72d1570d67178519316ac691dca34d53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4961c418ea426a925e8304ae1760c584",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "d4d91ea2fa0a8bcbf197",
+    "url": "/MyPortfolio/static/css/main.f7b4b746.chunk.css"
+  },
+  {
+    "revision": "8ed21229d88dfd6fe2c0",
+    "url": "/MyPortfolio/static/js/2.140508ac.chunk.js"
+  },
+  {
+    "revision": "d4d91ea2fa0a8bcbf197",
+    "url": "/MyPortfolio/static/js/main.af0c01e2.chunk.js"
+  },
+  {
+    "revision": "6049c315c2cc2d141cd0",
+    "url": "/MyPortfolio/static/js/runtime-main.c480b94a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MyPortfolio/precache-manifest.3b1988150f9297558fe53c68fe7ae12f.js"
+  "/MyPortfolio/precache-manifest.72d1570d67178519316ac691dca34d53.js"
 );
 
 self.addEventListener('message', (event) => {
