b8701bdd2f1d63ed8f1749396c6e24bec9e5570a
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-11-02 21:52:05 +0300

    refactoring

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -3,20 +3,18 @@ import styles from './Skills.module.css';
 import Skill from "./skill";
 
 function Skills() {
-  return (
-    <div className={styles.skills}>
-      <div className={styles.header}>
-          <span>Мои скилы</span>
-          <div className={styles.mainContent}>
-
-              <Skill/>
-              <Skill/>
-              <Skill/>
-
-          </div>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.skills}>
+            <div className={styles.header}>
+                <span>Мои скилы</span>
+                <div className={styles.mainContent}>
+                    <Skill/>
+                    <Skill/>
+                    <Skill/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Skills;
