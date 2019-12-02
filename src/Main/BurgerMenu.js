16d0c99359e8a9273822fc33ccf3c75c811f2b8d
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-12-02 21:27:00 +0300

    refactoring code

--- src/Main/BurgerMenu.js
+++ src/Main/BurgerMenu.js
@@ -5,6 +5,7 @@ import   './BurgerMenu.css';
 
 
 class  BurgerMenu extends React.Component {
+
     state ={
         activeForA: false,
         activeForNav: false
@@ -13,64 +14,39 @@ class  BurgerMenu extends React.Component {
 
     classForA = (e) => {
         e.preventDefault()
-        if(this.state.activeForA === false){
+        if (this.state.activeForA === false) {
             this.setState({
                 activeForA: true,
                 activeForNav: true
             })
-        }else{
+        } else {
             this.setState({
                 activeForA: false,
                 activeForNav: false
-
             })
         }
-
-
     };
 
-    // classForNav = (e) => {
-    //     e.preventDefault()
-    //     if(this.state.activeForNav === false){
-    //         this.setState({
-    //             activeForNav: true
-    //         })
-    //     }else{
-    //         this.setState({
-    //             activeForNav: false
-    //         })
-    //     }
-    //
-    //
-    // };
 
  render() {
+
         let classForA = this.state.active ? 'menu-btn menu-btn_active' : 'menu-btn' ;
         let classForNav = this.state.activeForNav ? 'menu-nav menu-nav_active' : 'menu-nav' ;
+
      return (
-
-
          <div className="section">
              <div className="menu-block">
                  <nav className={classForNav} >
-                     <a href="#">Main</a>
-                     <a href="#">Portfolio</a>
-                     <a href="#">About</a>
-                     <a href="#">Contacts</a>
+                     <a href="#">HOME</a>
+                     <a href="#">SKILLS</a>
+                     <a href="#">PROJECT</a>
+                     <a href="#">CONTACTS</a>
                  </nav>
-
                  <a href='#' className={classForA} onClick={this.classForA} >
                      <span></span>
                  </a>
-
              </div>
          </div>
-
-         // <div className='section' >
-         //     <a href='#' className={classForA} onClick={this.classForA} >
-         //         <span></span>
-         //     </a>
-         // </div>
      );
  }
 }
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -4,7 +4,7 @@ import Skill from "./Skill";
 import logo_js from ".././img/js_img.png"
 import logo_react from ".././img/react_img.png"
 import logo_angular from ".././img/angular.png"
-import logo_css from ".././img/CSS.png"
+import logo_css from ".././img/css.png"
 import logo_html from ".././img/html.ico"
 import logo_redux from ".././img/redux.png"
 
@@ -67,73 +67,14 @@ class  Skills extends React.Component {
 
         const MySkill = this.state.skill.map(sk => sk.map((s) => <Skill id={s.id} backgroundImage={s.backgroundImage} title={s.title} text={s.text}/>))
 
-
-        // const logoJS = {
-        //     backgroundImage: 'url(' + logo_js + ')',
-        // };
-        //
-        // const logoReact = {
-        //     backgroundImage: 'url(' + logo_react + ')',
-        // };
-        //
-        // const logoAngular = {
-        //     backgroundImage: 'url(' + logo_angular + ')',
-        // };
-        //
-        // const logoCss = {
-        //     backgroundImage: 'url(' + logo_css + ')',
-        // };
-        //
-        // const logoHtml = {
-        //     backgroundImage: 'url(' + logo_html + ')',
-        // };
-        //
-        // const logoRedux = {
-        //     backgroundImage: 'url(' + logo_redux + ')',
-        // };
-
-
-
-
-
         return (
             <div className={styles.skills}>
                 <div className={styles.header}>
                     <span className={styles.span__title}>Работа с несколькими языками  программирования и фреймворками</span>
                     <div className={styles.mainContent}>
-                        {/*<Skill style={logoJS}*/}
-                        {/*       title='разработка на Javascript'*/}
-                        {/*       text='JS'*/}
-                        {/*/>*/}
-
-                        {/*<Skill style={logoReact}*/}
-                        {/*       title='разработка на React'*/}
-                        {/*       text='React'*/}
-                        {/*/>*/}
-
-                        {/*<Skill style={logoAngular}*/}
-                        {/*       title='Использование фреймворка Angular'*/}
-                        {/*       text='Angular'*/}
-                        {/*/>*/}
-                        {/*<Skill style={logoCss}*/}
-                        {/*       title='Написание стилей'*/}
-                        {/*       text='CSS'*/}
-                        {/*/>*/}
-                        {/*<Skill style={logoHtml}*/}
-                        {/*       title='Использование языка разметки HTML'*/}
-                        {/*       text='HTML'*/}
-                        {/*/>*/}
-                        {/*<Skill style={logoRedux}*/}
-                        {/*       title='Использование библиотеки Redux'*/}
-                        {/*       text='Redux'*/}
-                        {/*/>*/}
-
                         <button onClick={this.prevSlide} disabled={this.state.disablePrev}>prev</button>
-
-                         {MySkill[this.state.currentIndex]}
-
+                        {MySkill[this.state.currentIndex]}
                         <button onClick={this.nextSlide} disabled={this.state.disableNext}>next</button>
-
                     </div>
                 </div>
             </div>
